05e95535c77ca137d4bbb8957b76d9bf9a25127a
Nikolay Averin <nikolay@miro.com>
2020-02-13 13:22:36 +0500

    left panel

--- template-builder/sidebar.js
+++ template-builder/sidebar.js
@@ -1,31 +1,23 @@
 miro.onReady(() => {
   // subscribe on user selected widgets
-  miro.addListener(miro.enums.event.SELECTION_UPDATED, getWidget)
-  getWidget()
+  miro.addListener(miro.enums.event.WIDGETS_CREATED, updateList)
+  miro.addListener(miro.enums.event.WIDGETS_DELETED, updateList)
+  miro.addListener(miro.enums.event.WIDGETS_TRANSFORMATION_UPDATED, updateList)
+
+  updateList()
 })
 
 // Get html elements for tip and text container
 const tipElement = document.getElementById('tip')
 const widgetTextElement = document.getElementById('widget-text')
 
-async function getWidget() {
-  // Get selected widgets
-  let widgets = await miro.board.selection.get()
+document.getElementById("build").addEventListener("click", () => drawTemplate())
 
-  // Get first widget from selected widgets
-  let text = widgets[0].text
+function drawTemplate() {
+    alert('drawTemplate')
+}
 
-  // Check that widget has text field
-  if (typeof text === 'string') {
-
-    // hide tip and show text in sidebar
-    tipElement.style.opacity = '0'
-    widgetTextElement.value = text
-  } else {
-
-    // show tip and clear text in sidebar
-    tipElement.style.opacity = '1'
-    widgetTextElement.value = ''
-  }
+function updateList() {
+  alert('updateList')
 }
 
