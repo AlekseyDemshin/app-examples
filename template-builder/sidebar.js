e2c3bae8bc60b2f7a8d037af89b6b399087cdd10
Nikolay Averin <nikolay@miro.com>
2020-02-13 16:53:46 +0500

    list in panel

--- template-builder/sidebar.js
+++ template-builder/sidebar.js
@@ -1,21 +1,54 @@
 miro.onReady(() => {
-    // subscribe on user selected widgets
-    miro.addListener(miro.enums.event.WIDGETS_CREATED, updateList)
-    miro.addListener(miro.enums.event.WIDGETS_DELETED, updateList)
-    //miro.addListener(miro.enums.event.WIDGETS_TRANSFORMATION_UPDATED, updateList)
+    miro.addListener('WIDGETS_CREATED', updateList)
+    miro.addListener('WIDGETS_DELETED', updateList)
+    miro.addListener('WIDGETS_TRANSFORMATION_UPDATED', updateList)
+
+    updateList()
 })
-
-const rowHeight = 100
-const columnWidth = 150
-
-// Get html elements for tip and text container
-const tipElement = document.getElementById('tip')
 
 function drawTemplate() {
     createMatrix()
 }
 
-function updateList() {
-    console.log('updateList')
+async function updateList() {
+    console.log("collect")
+    let widgets = await miro.board.widgets.get({
+        type: 'sticker'
+    });
+    widgets = widgets.filter(w => w.x > 0 && w.x < 1200 && w.y > 0 && w.y < 800)
+
+    console.log(widgets)
+    createTable("Results", "Looks like the matrix is empty. Drag stickers into it", widgets)
+}
+
+function createTable(title, emptyText, data) {
+    const container = document.getElementById("stat-container");
+    while (container.firstChild)  {
+        container.removeChild(container.firstChild);
+    }
+
+    const statView = document.createElement("div")
+    statView.className = "stat-list__table"
+
+    const titleView = document.createElement("div");
+    titleView.className = "stat-list__title";
+    titleView.innerHTML = `<span>${title}</span>`;
+    statView.appendChild(titleView);
+
+    if (data.size === 0){
+        const emptyView = document.createElement('div')
+        emptyView.className = "stat-list__empty"
+        emptyView.innerText = emptyText
+        statView.appendChild(emptyView);
+    } else {
+        data.forEach(w => {
+            let itemView = document.createElement("div");
+            itemView.className = "stat-list__item";
+            itemView.innerHTML = `<span class="stat-list__item-name">${w.text}</span>`
+                // + `<span class="stat-list__item-value">${w.text}</span>`;
+            statView.appendChild(itemView)
+        })
+    }
+    container.appendChild(statView);
 }
 
