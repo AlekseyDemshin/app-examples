8dc09f211fd1566906b55b60acf72dd4583010da
Nikolay Averin <nikolay@miro.com>
2020-02-13 19:16:37 +0500

    auto painting

--- template-builder/sidebar.js
+++ template-builder/sidebar.js
@@ -10,14 +10,26 @@ function drawTemplate() {
     createMatrix()
 }
 
+let widgets = [];
+
 async function updateList() {
     console.log("collect")
-    let widgets = await miro.board.widgets.get({
+    widgets = await miro.board.widgets.get({
         type: 'sticker'
     });
     widgets = widgets
         .filter(w => w.x > 0 && w.x < 1200 && w.y > 0 && w.y < 800)
-        .sort((l,r) => (800 - l.y) / (l.x - 0) - (800 - r.y) / (r.x - 0))
+        .map(w => {
+            w.ieRatio = (800 + 1 - w.y) / (w.x + 1 - 0);
+            return w;
+        })
+        .sort((l, r) => r.ieRatio - l.ieRatio);
+
+    //normalize
+    if (widgets.length > 0) {
+        const maxRatio = widgets[0].ieRatio;
+        widgets.forEach(w => w.ieRatioNormalized = w.ieRatio / maxRatio);
+    }
 
     console.log(widgets)
     createTable("Results", "Looks like the matrix is empty. Drag stickers into it", widgets)
@@ -25,7 +37,7 @@ async function updateList() {
 
 function createTable(title, emptyText, data) {
     const container = document.getElementById("stat-container");
-    while (container.firstChild)  {
+    while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 
@@ -37,7 +49,7 @@ function createTable(title, emptyText, data) {
     titleView.innerHTML = `<span>${title}</span>`;
     statView.appendChild(titleView);
 
-    if (data.size === 0){
+    if (data.size === 0) {
         const emptyView = document.createElement('div')
         emptyView.className = "stat-list__empty"
         emptyView.innerText = emptyText
@@ -46,11 +58,26 @@ function createTable(title, emptyText, data) {
         data.forEach(w => {
             let itemView = document.createElement("div");
             itemView.className = "stat-list__item";
-            itemView.innerHTML = `<span class="stat-list__item-name">${w.text} |</span>`
-                 + `<span class="stat-list__item-value">${(800 - w.y) / (w.x - 0)}</span>`;
+            itemView.innerHTML = `<span class="stat-list__item-name">${w.text} | </span>`
+                + `<span class="stat-list__item-value">${w.ieRatioNormalized} | </span>`
+                + `<span class="stat-list__item-value">${Math.ceil(w.ieRatioNormalized / 0.2) - 1}</span>`;
+            itemView.onclick = function () {
+                miro.board.selection.clear();
+                miro.board.selection.selectWidgets(w.id);
+                moveViewPort(-200, -100, 1300, 1300)
+            };
             statView.appendChild(itemView)
         })
     }
     container.appendChild(statView);
+}
+
+
+const colors = ['#F5F6F8', '#FFF9B2', '#F5D22B', '#FFA75B', '#F38090'];
+
+function paint() {
+    const step = 1 / colors.length;
+    widgets.forEach(w => w.style.stickerBackgroundColor = colors[Math.ceil(w.ieRatioNormalized / step) - 1])
+    miro.board.widgets.update(widgets);
 }
 
