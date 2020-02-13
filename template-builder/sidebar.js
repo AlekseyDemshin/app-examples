accb9b4f722489e8a5b799ec0431d5a477810bfe
Nikolay Averin <nikolay@miro.com>
2020-02-13 13:45:18 +0500

    left panel

--- template-builder/sidebar.js
+++ template-builder/sidebar.js
@@ -1,23 +1,46 @@
 miro.onReady(() => {
-  // subscribe on user selected widgets
-  miro.addListener(miro.enums.event.WIDGETS_CREATED, updateList)
-  miro.addListener(miro.enums.event.WIDGETS_DELETED, updateList)
-  miro.addListener(miro.enums.event.WIDGETS_TRANSFORMATION_UPDATED, updateList)
+    // subscribe on user selected widgets
+    miro.addListener(miro.enums.event.WIDGETS_CREATED, updateList)
+    miro.addListener(miro.enums.event.WIDGETS_DELETED, updateList)
+    //miro.addListener(miro.enums.event.WIDGETS_TRANSFORMATION_UPDATED, updateList)
 
-  updateList()
+    updateList()
 })
+
+const rowHeight = 100
+const columnWidth = 150
 
 // Get html elements for tip and text container
 const tipElement = document.getElementById('tip')
-const widgetTextElement = document.getElementById('widget-text')
 
-document.getElementById("build").addEventListener("click", () => drawTemplate())
+// document.getElementById("build").addEventListener("click", () => drawTemplate())
 
 function drawTemplate() {
-    alert('drawTemplate')
+    const widgets = []
+
+    for (let i = 0; i < 5; i++) {
+        const colX = i * columnWidth + (2 * i);
+        widgets.push(getShape(colX, 100))
+    }
+
+    miro.board.widgets.create(widgets)
+}
+
+function getShape(x, y) {
+    return {
+        type: 'shape',
+        x: x,
+        y: y,
+        width: columnWidth,
+        height: rowHeight,
+        style: {
+            borderWidth: 0,
+            backgroundColor: '#B2EEE4'
+        }
+    }
 }
 
 function updateList() {
-  alert('updateList')
+    alert('updateList')
 }
 
