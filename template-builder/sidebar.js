096e7e4231972d775fe0e7577e203b122fe45fa3
Nikolay Averin <nikolay@miro.com>
2020-02-14 17:58:50 +0500

    animation matrix

--- template-builder/sidebar.js
+++ template-builder/sidebar.js
@@ -20,6 +20,7 @@ async function init() {
 
 
 async function drawTemplate() {
+    animateMatrix();
     // delete old matrix
     await createMatrix()
 }
@@ -136,6 +137,62 @@ function createTable(matrix, widgets) {
             '',
             '');
     })
+}
+
+function animateMatrix() {
+    var c = document.getElementById("c");
+    c.hidden = false;
+    var ctx = c.getContext("2d");
+
+    //making the canvas full screen
+    c.height = window.innerHeight;
+    c.width = window.innerWidth;
+
+    var chars = "jg9gj583hegi845gniewrgli;g04jsu84k$%*";
+    //converting the string into an array of single characters
+    chars = chars.split("");
+
+    var font_size = 10;
+    var columns = c.width/font_size; //number of columns for the rain
+    //an array of drops - one per column
+    var drops = [];
+    //x below is the x coordinate
+    //1 = y co-ordinate of the drop(same for every drop initially)
+    for(var x = 0; x < columns; x++)
+        drops[x] = 1;
+
+    //drawing the characters
+    function draw()
+    {
+        //Black BG for the canvas
+        //translucent BG to show trail
+        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+        ctx.fillRect(0, 0, c.width, c.height);
+
+        ctx.fillStyle = "#0F0"; //green text
+        ctx.font = font_size + "px arial";
+        //looping over drops
+        for(var i = 0; i < drops.length; i++)
+        {
+            //a random chars character to print
+            var text = chars[Math.floor(Math.random()*chars.length)];
+            //x = i*font_size, y = value of drops[i]*font_size
+            ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+            //sending the drop back to the top randomly after it has crossed the screen
+            //adding a randomness to the reset to make the drops scattered on the Y axis
+            if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                drops[i] = 0;
+
+            //incrementing Y coordinate
+            drops[i]++;
+        }
+    }
+    let interval = setInterval(draw, 33);
+    setTimeout(() => {
+        clearInterval(interval);
+        c.hidden = true;
+    }, 2500)
 }
 
 async function removeFromTable(widgets) {
